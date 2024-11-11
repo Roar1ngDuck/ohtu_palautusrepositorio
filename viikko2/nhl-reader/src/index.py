b0f82cc0d5ba0ec5cda5fcf5cc32e87050867304
niko <niko.summanen1@gmail.com>
2024-11-11 10:51:55 +0200

    Ex 11

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -5,20 +5,17 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
     players = []
 
     for player_dict in response:
         player = Player(player_dict)
         players.append(player)
 
-    print("Oliot:")
+    print("Players from FIN:")
 
-    for player in players:
-        if (player.nationality == "FIN"):
-            print(player)
+    finnish_players = filter(lambda player: player.nationality == "FIN", players)
+    for player in sorted(finnish_players, key=lambda player: player.goals + player.assists, reverse=True):
+        print(player)
 
 if __name__ == "__main__":
     main()
